a042f66869be439678bc295acbde91aa77dcf182
huangwentao <huangwentao@dianchu.com>
2022-07-25 22:10:53 +0800

    feat: 🎸 测试主应用husky

--- one/src/main.js
+++ one/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-06 11:03:25
- * @LastEditTime: 2020-12-06 11:54:59
+ * @LastEditTime: 2022-07-25 22:01:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /one/src/main.js
@@ -19,7 +19,7 @@ Vue.config.productionTip = false
 
 let router = null
 let instance = null
-
+console.log('微应用')
 function render() {
   router = new VueRouter({
     base: window.__POWERED_BY_QIANKUN__ ? 'one' : '/',
